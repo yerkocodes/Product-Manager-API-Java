837faf593882a4bb303559c95082a9319d979559
Yerko Cortés Baeza <holayerkocodes@gmail.com>
2025-04-16 20:07:51 -0400

    Inicio de proyecto base

---
+++ src/main/java/com/yerkocortes/productmanagerapi/ProductManagerApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.yerkocortes.productmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yerkocortes/productmanagerapi/ProductManagerApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yerkocortes.productmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
